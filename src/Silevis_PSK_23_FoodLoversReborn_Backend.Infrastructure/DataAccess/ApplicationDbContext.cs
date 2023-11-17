8c2236e0e0f971a81cc5fd60ac670f199f20771a
Zimeek <szymczykm6936@o2.pl>
2023-11-17 21:45:46 +0100

    Add DbSets

--- src/Silevis_PSK_23_FoodLoversReborn_Backend.Infrastructure/DataAccess/ApplicationDbContext.cs
+++ src/Silevis_PSK_23_FoodLoversReborn_Backend.Infrastructure/DataAccess/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Silevis_PSK_23_FoodLoversReborn_Backend.Domain.Entities;
 
 namespace Silevis_PSK_23_FoodLoversReborn_Backend.Infrastructure.DataAccess;
 
@@ -7,4 +9,11 @@ public class ApplicationDbContext : DbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     { }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+    
+    public DbSet<Internship> Internships { get; set; }
 }
