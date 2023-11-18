720851ca8951752c52d8cab8760f51b8088b93f1
Adam <MorfeuszowyMorfeusz@gmail.com>
2023-11-18 04:53:01 +0100

    Added documents controller

---
+++ src/Silevis_PSK_23_FoodLoversReborn_Backend.API/Controllers/DocumentsController.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Silevis_PSK_23_FoodLoversReborn_Backend.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DocumentsController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+    private readonly IMediator _mediator;
+    
+    public DocumentsController(IConfiguration configuration, IMediator mediator)
+    {
+        _configuration = configuration;
+        _mediator = mediator;
+    }
+    
+    [HttpPost("Upload")]
+    public async Task<IActionResult> Upload()
+    {
+        return Ok();
+    }
+    
+    [HttpGet("GetStatus")]
+    public async Task<IActionResult> GetStatus()
+    {
+        return Ok();
+    }
+    
+}
