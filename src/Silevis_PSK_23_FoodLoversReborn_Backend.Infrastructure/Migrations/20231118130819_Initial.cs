3e13960094d48179129b867365df7f4600469b13
Zimeek <szymczykm6936@o2.pl>
2023-11-18 14:25:01 +0100

    Add migration

---
+++ src/Silevis_PSK_23_FoodLoversReborn_Backend.Infrastructure/Migrations/20231118130819_Initial.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Silevis_PSK_23_FoodLoversReborn_Backend.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "InternshipDelayRequests",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    RequestedDateStart = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RequestedDateEnd = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Approved = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InternshipDelayRequests", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Internships",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    Passed = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Internships", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudentDocuments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FilePath = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    DocumentId = table.Column<int>(type: "int", nullable: false),
+                    AlbumNumber = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentDocuments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InternshipDetails",
+                columns: table => new
+                {
+                    InternshipId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CompanyAddress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CompanyPhone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateStart = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateEnd = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Month = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InternshipDetails", x => x.InternshipId);
+                    table.ForeignKey(
+                        name: "FK_InternshipDetails_Internships_InternshipId",
+                        column: x => x.InternshipId,
+                        principalTable: "Internships",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InternshipDelayRequests");
+
+            migrationBuilder.DropTable(
+                name: "InternshipDetails");
+
+            migrationBuilder.DropTable(
+                name: "StudentDocuments");
+
+            migrationBuilder.DropTable(
+                name: "Internships");
+        }
+    }
+}
